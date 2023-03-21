cf6b9a7f690a0fc99210fdfea58a2ea146e9dd3c
Adrian Mudzwiti <54492734+archangeladrian@users.noreply.github.com>
2023-03-21 20:50:10 +0200

    Update error handling and refactor code.

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,8 @@
 import boto3
+import botocore
 import os
 import tempfile
+import time
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 s3 = boto3.resource('s3')
@@ -23,30 +25,27 @@ def lambda_handler(event, context):
         bucket = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
         
-        try:
-            # Download the object from s3
-            s3.meta.client.download_file(bucket, key, key)
-        except FileNotFoundError:
-            print(f"The file {key} does not exist")
-       
         # Get file name from key, join /tmp folder path and file name
         file_name = key
         upload_file_path = os.path.join(temFilePath, file_name)
         
-        def upload_to_blob_storage(file_path, file_name):
-            """Upload file to Azure storage as blob from /tmp folder"""
-            blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-            blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
+        try:
+            # Download the object from s3
+            s3.meta.client.download_file(bucket, key, key)
             
-            with open(file_path, "rb") as data:
-                blob_client.upload_blob(data, overwrite=True)
-                print(f" {file_name} uploaded to Azure Blob !")
+            def upload_to_blob_storage(file_path, file_name):
+                """Upload file to Azure storage as blob from /tmp folder"""
+                blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+                blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
                 
-        upload_to_blob_storage(upload_file_path, file_name)
-  
-        
-        
-        
-        
-        
-    
+                with open(file_path, "rb") as data:
+                    blob_client.upload_blob(data, overwrite=True)
+                    print(f" {file_name} uploaded to Azure Blob !")
+                    
+            upload_to_blob_storage(upload_file_path, file_name)
+                    
+        except FileNotFoundError:
+            print(f"The file {key} does not exist")
+        except botocore.exceptions.ClientError as error:
+            print(f"Error : {error.response['Error']['Code']}, '{upload_file_path}' not found." )
+            pass
