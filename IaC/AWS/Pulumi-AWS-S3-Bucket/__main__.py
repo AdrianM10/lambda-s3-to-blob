e7061f20b8c90c2a4fabc069217e61dbfc9a4b32
Adrian Mudzwiti <54492734+archangeladrian@users.noreply.github.com>
2023-03-24 08:03:57 +0200

    Add folder structure for lambda layer and requirements.txt file

--- IaC/AWS/Pulumi-AWS-S3-Bucket/__main__.py
+++ IaC/AWS/Pulumi-AWS-S3-Bucket/__main__.py
@@ -2,9 +2,13 @@
 
 import pulumi
 from pulumi_aws import s3
+import pulumi_aws as aws
 
+
+bucket_name = 'journeytothewest'
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+bucket = s3.Bucket(bucket_name,
+                   )
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
